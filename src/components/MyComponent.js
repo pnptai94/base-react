2b3d7182f660a1dbd6e88985fafa660bf83b9986
pnptai94 <pnptai.cits@gmail.com>
2024-06-15 20:14:43 +0700

    150620240814

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,20 @@ class MyComponent extends React.Component {
         address: "An Giang",
         age: 31
     };
-
+    handleClickme(event) {
+        //console.log(">>> you click button Click me");
+        console.log(event);
+    }
+    handleMouseover(event) {
+        // console.log(">>> you click button Hover me");
+        console.log(event);
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} ! Address: {this.state.address}
+                <button onClick={this.handleClickme}>click me</button>
+                <button onMouseOver={this.handleMouseover}>Hover me</button>
             </div>
         );
     }
