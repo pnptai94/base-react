4d619549f8c7461000a8e86b85dfe1a6e233ed40
pnptai94 <pnptai.cits@gmail.com>
2024-07-10 20:33:59 +0700

    1007240833

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,18 @@
 import React from "react";
 import UserInfo from "./UserInfo";
+import Displayinfo from "./Displayinfo";
 
 class MyComponent extends React.Component {
 
     render() {
+        const Myinfo = ['tai', 'nam', 'ngoc']
         return (
             <div>
-                <UserInfo></UserInfo>
+                <UserInfo />
+                <br /><br />
+                <Displayinfo name="PhuocTai" age="32" />
+                <hr />
+                <Displayinfo name="HoangNam" age="33" Myinfo={Myinfo} />
             </div>
         );
     }
